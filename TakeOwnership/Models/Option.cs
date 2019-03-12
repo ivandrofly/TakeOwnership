fab9fb34da87636e8ae27b3d713178456fd1a291
Ivandro Ismael <ivandrofly@gmail.com>
2019-03-12 18:40:52 +0000

    Add project files.

---
+++ TakeOwnership/Models/Option.cs
@@ -0,0 +1,35 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TakeOwnership.Models
+{
+    class Option
+    {
+        [Option('t', "target", Required = true, HelpText ="The target directory to be processed")]
+        /// <summary>
+        /// Target path
+        /// </summary>
+        public string Target { get; set; }
+
+        [Option('f', "file", HelpText = "True if files should be processed")]
+        /// <summary>
+        /// True if files should be processed
+        /// </summary>
+        public bool File { get; set; }
+
+        [Option('f', "file", HelpText = "True if files should be processed")]
+        /// <summary>
+        /// True if files should be processed
+        /// </summary>
+        public bool Directory { get; set; }
+
+        [Option('r', "recursive", HelpText = "True if all sub-directories should be processed")]
+
+        /// <summary>
+        /// True if all sub-directories should be processed
+        /// </summary>
+        public bool IsRecursive { get; set; }
+    }
+}
---
+++ TakeOwnership/Program.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+using System;
+using TakeOwnership.Models;
+
+namespace TakeOwnership
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Option>(args)
+                .WithParsed<Option>(opt =>
+                {
+
+                });
+        }
+    }
+}
